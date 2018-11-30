a5df3ea0709d13951fd8be4c8677cd43ceddf438
Lisayg <40559457+Lisayg@users.noreply.github.com>
2018-11-30 22:30:46 +0900

    Update MainMenu.js

--- script/menu/MainMenu.js
+++ script/menu/MainMenu.js
@@ -13,7 +13,7 @@ function generateMainMenuLabels(headerText){
             "100px"
         ),
         new Label(
-            "MIT License © designed by Diligitis, 2018", 
+            "MIT License © designed by Diligitis.org 2018", 
             new Vector2(900,730),
             Vector2.zero,
             "white",
